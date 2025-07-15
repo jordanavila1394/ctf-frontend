2f409b1ed58d677cd1f1d0224217c961698ca284
Avila George <george.avila@external.bper.it>
2025-07-15 15:32:19 +0200

    aggiunta

--- src/app/helpers/loader.interceptor.ts
+++ src/app/helpers/loader.interceptor.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
 import {
-    HttpEvent, HttpHandler, HttpInterceptor, HttpRequest
+    HttpEvent,
+    HttpHandler,
+    HttpInterceptor,
+    HttpRequest
 } from '@angular/common/http';
-import { Observable, finalize } from 'rxjs';
+import { Observable, finalize, race, timer, filter } from 'rxjs';
 import { LoaderService } from '../services/loader.service';
 
 @Injectable()
@@ -11,7 +14,13 @@ export class LoaderInterceptor implements HttpInterceptor {
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         this.loaderService.show();
-        return next.handle(req).pipe(
+
+        const request$ = next.handle(req);
+        const timeout$ = timer(10000);
+
+        return race([request$, timeout$]).pipe(
+            // Filtro solo HttpEvent (evita che passi il valore "0" del timer)
+            filter((event): event is HttpEvent<any> => typeof event !== 'number'),
             finalize(() => this.loaderService.hide())
         );
     }
