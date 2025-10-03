c9ad5bbdff2ef1d4a5fcf1ed9a3973c1a2ca57c8
Avila George <george.avila@external.bper.it>
2025-10-03 12:59:33 +0200

    add attendances

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -92,7 +92,7 @@ import { AuthGuard } from './services/auth.guard';
                                 ).then((m) => m.AttendanceModule),
                             canActivate: [AuthGuard],
                             data: {
-                                roles: ['ROLE_ADMIN', 'ROLE_MODERATOR'],
+                                roles: ['ROLE_ADMIN', 'ROLE_MODERATOR','ROLE_PREPOSTO'],
                             },
                         },
                         {
